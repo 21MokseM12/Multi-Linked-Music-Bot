8fc40fa7f7d7b4d44194a014c03925229d6d5613
21MokseM12 <maksim.buynov.00@mail.ru>
2024-06-27 00:44:44 +0300

    common: update LinkValidator class structure for each of types of link

--- src/main/java/com/app/utils/LinkValidator.java
+++ src/main/java/com/app/utils/LinkValidator.java
@@ -9,8 +9,12 @@ public final class LinkValidator {
     public static boolean isValidLink(String link, StreamServiceType linkType) {
         return switch(linkType) {
             case YANDEX_MUSIC_LINK -> isValidYandexMusicLink(link);
+            case SPOTIFY_MUSIC_LINK -> isValidSpotifyMusicLink(link);
             case APPLE_MUSIC_LINK -> isValidAppleMusicLink(link);
             case YOUTUBE_MUSIC_LINK -> isValidYouTubeMusicLink(link);
+            case GOOGLE_PLAY_MUSIC_LINK -> isValidGooglePlayMusicLink(link);
+            case SOUNDCLOUD_MUSIC_LINK -> isValidSoundCloudMusicLink(link);
+            case SHAZAM_MUSIC_LINK -> isValidShazamMusicLink(link);
             case INVALID_LINK -> false;
         };
     }
@@ -19,11 +23,27 @@ public final class LinkValidator {
         return false;
     }
 
+    private static boolean isValidSpotifyMusicLink(String link) {
+        return false;
+    }
+
     private static boolean isValidAppleMusicLink(String link) {
         return false;
     }
 
     private static boolean isValidYouTubeMusicLink(String link) {
+        return false;
+    }
+
+    private static boolean isValidGooglePlayMusicLink(String link) {
+        return false;
+    }
+
+    private static boolean isValidSoundCloudMusicLink(String link) {
+        return false;
+    }
+
+    private static boolean isValidShazamMusicLink(String link) {
         return false;
     }
 }
