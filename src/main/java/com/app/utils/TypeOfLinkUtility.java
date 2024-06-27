d23adc82ccb5f9fbac216e9d764702a7b4839928
21MokseM12 <maksim.buynov.00@mail.ru>
2024-06-27 19:51:47 +0300

    features: - delete Google Play Music from enumeration class - redirect the Validator implementation - add to types of links utility class new types of link

--- src/main/java/com/app/utils/TypeOfLinkUtility.java
+++ src/main/java/com/app/utils/TypeOfLinkUtility.java
@@ -10,9 +10,12 @@ public final class TypeOfLinkUtility {
         if (splittedLink.length < 2) return StreamServiceType.INVALID_LINK;
 
         return switch (splittedLink[1]) {
+            case "open.spotify.com" -> StreamServiceType.SPOTIFY_MUSIC_LINK;
             case "music.yandex.ru" -> StreamServiceType.YANDEX_MUSIC_LINK;
             case "music.apple.com" -> StreamServiceType.APPLE_MUSIC_LINK;
-            case "www.youtube.com" -> StreamServiceType.YOUTUBE_MUSIC_LINK;
+            case "music.youtube.com" -> StreamServiceType.YOUTUBE_MUSIC_LINK;
+            case "soundcloud.com" -> StreamServiceType.SOUNDCLOUD_MUSIC_LINK;
+            case "www.shazam.com" -> StreamServiceType.SHAZAM_MUSIC_LINK;
             default -> StreamServiceType.INVALID_LINK;
         };
     }
